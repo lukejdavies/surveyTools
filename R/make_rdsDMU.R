66024b75fbba42a32da000077fae4ac94a63b79e
lukejdavies <luke.j.davies@uwa.edu.au>
2020-08-12 12:25:48 +0800

    Changes to doucumentation for all functions

--- R/make_rdsDMU.R
+++ R/make_rdsDMU.R
@@ -3,7 +3,7 @@
 #' @description Takes a catalogue and various meta data/column information and writes out a standardised Data Management Unit (DMU) data product as a .rds file.
 #'
 #' @param DMUName Name for the DMU
-#' @param cat data.frame of catalogue data to add to DMU
+#' @param cat data.frame of catalogue data to add to DMU. Column names should be correct for your DMU file.
 #' @param summary a 1/2 sentence overview description of your DMU
 #' @param usrGen name of person who is making DMU
 #' @param contactGen email contact of person who is making DMU
@@ -13,12 +13,12 @@
 #' @param colucd a vector containing string Universal Content Descriptors (UCDs) for each column in cat (see http://cdsweb.u-strasbg.fr/UCD/tree/js/)
 #' @param colunits a vector containing string units for each column in cat
 #' @param README # a string README for the catalogue (please separate lines with \n
+#' @param add list of additional sublists you wish to be added to the DMU. 
 #' @param test TRUE/FLASE - If true catalogue can be run with dummy values (see example) and not give errors
 #' @return a list containing the DMU. The DMU will also be written out as a .rds file with the format: DMUName,'_',format(Sys.time(), "%d_%m_%Y"),'_v',version,'.rds'
 #' @author L. Davies <luke.j.davies@uwa.edu>
-#' @examples
-# DMUName<-'dummy'
-# cat<-data.frame(col1=round(runif(10,1,10)), col2=runif(10,0,360), col3=runif(10,-90,90))
+#' @examples DMUName<-'dummy'
+#cat<-data.frame(col1=round(runif(10,1,10)), col2=runif(10,0,360), col3=runif(10,-90,90))
 # summary<-'This is my catalogue, with things in it that I measured'
 #usrGen<-'G. M. Bluth'
 #contactGen<-'g.m.bluth@thebluthfamily.com'
@@ -27,7 +27,7 @@
 #coldescription<-c('This is column1, it has column1-like things in it - maybe an ID', 'This is column2, supprisingly it has column2-like things in it - maybe an RA', 'This is column2, supprisingly it has column2-like things in it - maybe a DEC' )
 #colucd<-c('meta.id', 'pos.eq.ra', 'pos.eq.dec')
 #colunits<-c('none', 'deg', 'deg')
-#README<-c('The is a README that describes this table. I can put in lots of information about hot the catalogue is generated and its providance. If I wane to start a new line, I should use \n. Or I can skip lines with \n\n. This way if someone wants to read it, they can easily do cat(DMU$README)')
+#README<-c('The is a README that describes this table.')
 #DMU<-make_rdsDMU(DMUName, cat, summary, usrGen, contactGen, scriptGen, version, coldescription,  colucd, colunits, README, test=TRUE)
 #' @export
 make_rdsDMU<-function(DMUName, cat, summary, usrGen, contactGen, scriptGen, version, coldescription,  colucd, colunits, README, add=NA, test=FALSE){
